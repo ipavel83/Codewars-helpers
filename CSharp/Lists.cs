11260d6774154fe55dcf98174c91bce1e4d5ad03
Pavel Ivanov <ipavel83@gmail.com>
2020-09-23 16:48:44 +0400

    Create Lists.cs
    Count repetitive elements and output them as enumerable sequence of tuples.

---
+++ CSharp/Lists.cs
@@ -0,0 +1,25 @@
+  using System;
+  using System.Linq;
+  using System.Collections.Generic;
+
+    public static IEnumerable<(T Item, int Count)> CountRepetition<T>(IEnumerable<T> source) where T : IComparable
+      {
+      if (source.Count() == 0)
+        yield break;
+      int count = 0;
+      T last = source.First();
+      foreach( var el in source)
+        {
+        if (el.CompareTo(last) != 0)
+          {
+          yield return (last, count);
+          last = el;
+          count = 1;
+          }
+        else
+          count++;
+        }
+      yield return (last, count);
+      }
+  }
+}

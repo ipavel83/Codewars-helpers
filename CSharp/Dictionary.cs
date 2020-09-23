1eb47dc0aba6a8412915a91d1922d76c52d67307
Pavel Ivanov <ipavel83@gmail.com>
2020-09-23 13:36:56 +0400

    Create Dictionary.cs

---
+++ CSharp/Dictionary.cs
@@ -0,0 +1,14 @@
+  public static Dictionary<TResult,int> CountElements<T, TResult>(
+                    List<T> seq, Func<T,TResult> keyFunc, Func<T,int> summator)
+    {
+    var counter = new Dictionary<TResult, int>();
+        foreach (var el in seq)
+          {
+            TResult key = keyFunc(el);
+            if (counter.ContainsKey(key))
+              counter[key]+= summator(el);
+            else 
+              counter.Add(key,summator(el));
+          }
+        return counter;
+    }

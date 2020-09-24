1a9573f21bf6ee0dfe5937bec3a213ba8ec5338d
Pavel Ivanov <ipavel83@gmail.com>
2020-09-24 15:56:49 +0400

    Update Lists.cs

--- CSharp/Lists.cs
+++ CSharp/Lists.cs
@@ -2,6 +2,12 @@
   using System.Linq;
   using System.Collections.Generic;
 
+    //Extention method
+    public static (T first, T second) FirstTwoToTuple<T>(this IEnumerable<T> items)
+    {
+        return (items.First(), items.Skip(1).First());
+    }
+
     public static IEnumerable<(T Item, int Count)> CountRepetition<T>(IEnumerable<T> source) where T : IComparable
       {
       if (source.Count() == 0)
